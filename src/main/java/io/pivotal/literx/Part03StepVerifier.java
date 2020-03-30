a1a6fea54d6af377288be2ce2afd3894563d607f
Guillaume <guillaume@guillaumegallois.com>
2020-03-30 12:38:05 +0200

    Exo

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,10 +16,15 @@
 
 package io.pivotal.literx;
 
-import java.util.function.Supplier;
-
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -33,29 +38,48 @@ public class Part03StepVerifier {
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.expectComplete()
+				.verify();
+		// "verifyComplete()" ou "expectComplete().verify()"
 	}
 
 //========================================================================================
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.expectError(RuntimeException.class)
+				.verify();
+		// "verifyError()" ou ".expectError(RuntimeException.class).verify()"
 	}
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite" username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		StepVerifier.FirstStep<User> sv = StepVerifier.create(flux);
+		sv
+			.assertNext(user -> assertThat(user.getUsername()).isEqualTo("swhite"))
+			.assertNext(user -> assertThat(user.getUsername()).isEqualTo("jpinkman"))
+			.verifyComplete();
+		// .expectNextMatches(user -> user.getUsername().equals("swhite"))
+		// .assertNext(user -> Assertions.assertThat(user.getUsername()).isEqualToIgnoringCase("jpinkman"))
+		// .verifyComplete();
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.FirstStep<Long> sv = StepVerifier.create(flux);
+		Duration duration = sv.expectNextCount(10).verifyComplete();
+		System.out.println(duration);
 	}
 
 //========================================================================================
@@ -63,7 +87,16 @@ public class Part03StepVerifier {
 	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
 	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		final Duration duration =
+			StepVerifier
+				.withVirtualTime(() -> Mono.delay(Duration.ofHours(1)))
+				.expectSubscription()
+				.expectNoEvent(Duration.ofSeconds(1))
+				.thenAwait(Duration.ofHours(1))
+				.expectNextCount(1)
+				.expectComplete()
+				.verify();
+		System.out.println(duration.toMillis());
 	}
 
 	private void fail() {
--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -15,25 +15,29 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(user -> capitalizeUser(user));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> capitalizeUser(user));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(user -> asyncCapitalizeFluxUser(user));
 	}
 
-	Mono<User> asyncCapitalizeUser(User u) {
-		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+	User capitalizeUser(User u) {
+		return new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase());
+	}
+
+	Flux<User> asyncCapitalizeFluxUser(User u) {
+		return Flux.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
 	}
 
 }
