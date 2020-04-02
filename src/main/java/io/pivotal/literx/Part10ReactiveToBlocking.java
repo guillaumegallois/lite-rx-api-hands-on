01b963e49c4ebc095187ff903e7ee68ba6a5d7ab
Guillaume <guillaume@guillaumegallois.com>
2020-04-02 11:55:13 +0200

    part10 & part11

--- src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
+++ src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
@@ -13,16 +13,25 @@ public class Part10ReactiveToBlocking {
 
 //========================================================================================
 
+//	The subscribeOn method allow to isolate a sequence from the start on a provided Scheduler. For example, the Schedulers.elastic() will create a pool
+//	of threads that grows on demand, releasing threads that haven't been used in a while automatically.
+//	Use that trick to slowly read all users from the blocking repository in the first exercise. Note that you will need to wrap the call to the
+//	repository inside a Flux.defer lambda.
 	// TODO Return the user contained in that Mono
 	User monoToValue(Mono<User> mono) {
-		return null;
+		return mono.block();
 	}
 
 //========================================================================================
 
+//	For slow subscribers (eg. saving to a database), you can isolate a smaller section of the sequence with the publishOn operator.
+//	Unlike subscribeOn, it only affects the part of the chain below it, switching it to a new Scheduler.
+//	As an example, you can use doOnNext to perform a save on the repository, but first use the trick above to isolate that save
+//	into its own execution context. You can make it more explicit that you're only interested in knowing if the save succeeded or failed
+//	by chaining the then() operator at the end, which returns a Mono<Void>.
 	// TODO Return the users contained in that Flux
 	Iterable<User> fluxToValues(Flux<User> flux) {
-		return null;
+		return flux.toIterable();
 	}
 
 }
--- src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
+++ src/main/java/io/pivotal/literx/Part11BlockingToReactive.java
@@ -25,16 +25,29 @@ public class Part11BlockingToReactive {
 
 //========================================================================================
 
+//	Sometimes you can only migrate part of your code to be reactive, and you need to reuse reactive sequences in more imperative code.
+//	Thus if you need to block until the value from a Mono is available, use Mono#block() method. It will throw an Exception if the onError event
+//	is triggered.
+//	Note that you should avoid this by favoring having reactive code end-to-end, as much as possible. You MUST avoid this at all cost in the middle
+//	of other reactive code, as this has the potential to lock your whole reactive pipeline.
 	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
 	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
-		return null;
+		return Flux.defer(() -> Flux.fromIterable(repository.findAll()))
+				.subscribeOn(Schedulers.elastic());
 	}
 
 //========================================================================================
 
-	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
+//	Similarly, you can block for the first or last value in a Flux with blockFirst()/blockLast().
+//	You can also transform a Flux to an Iterable with toIterable.
+//	Same restrictions as above still apply.
+	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void>
+	// that signal the end of the operation
 	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
-		return null;
+		return flux
+				.publishOn(Schedulers.elastic())
+				.doOnNext(repository::save)
+				.then();
 	}
 
 }
